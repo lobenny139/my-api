1885fbe647e5ae53e79c214c283f19afd4c4a5f6
羅智全 <96677946+lobenny139@users.noreply.github.com>
2024-04-06 14:21:50 +0800

    Create DemoApplication.java

---
+++ src/main/java/DemoApplication.java
@@ -0,0 +1,43 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Import({
+		// swagger config@local
+		com.my.api.config.SwaggerConfig.class,
+		// config@tom-db-service
+		com.my.db.config.EntityServiceConfig.class,
+})
+
+@ComponentScan	(basePackages = {
+		// controller@local
+		"com.my.api.controller",
+		// aop@local
+		"com.my.api.aop",
+		// aop@imbidgod-db-service
+		"com.imbidgod.db.service.aop",
+		// table access service@tom-db-service
+		"com.my.db.service.provider" ,
+})
+
+@EnableJpaRepositories(
+		// repositiry@tom-db-service
+		"com.my.db.repository"
+)
+
+@EntityScan(basePackages = {
+		// entity@my-db-entity
+		"com.my.db.entity"
+})
+
+@SpringBootApplication
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
+
